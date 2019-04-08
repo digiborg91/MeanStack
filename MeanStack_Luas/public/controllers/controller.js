8897b17468166167fa2dabd3bb4e153a357d37f8
digiborg91 <43547667+digiborg91@users.noreply.github.com>
2019-04-08 15:45:49 +0100

    Add files via upload

--- MeanStack_Luas/public/controllers/controller.js
+++ MeanStack_Luas/public/controllers/controller.js
@@ -1,11 +1,14 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ui.bootstrap']);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
-    console.log("Hello World from controller");
+    console.log("Hello World controller");
+
+    $scope.currentPage = 1;
+    $scope.pageSize = 5;
 
 
 var refresh = function() {
   $http.get('/luaslist').success(function(response) {
-    console.log("I got the data I requested");
+    console.log("Data Requested");
     $scope.luaslist = response;
     $scope.luas_stops_english = "";
   });
@@ -36,7 +39,7 @@ $scope.edit = function(id) {
 };  
 
 $scope.update = function() { //function needs fixed
-  console.log($scope.stop._id);
+  console.log($scope.luas_stops_english._id);  //Function fixed. replaced ($scope.stop._id) with ($scope.luas_stops_english._id);  
   $http.put('/luaslist/' + $scope.luas_stops_english._id, $scope.luas_stops_english).success(function(response) {
     refresh();
   })
@@ -46,4 +49,18 @@ $scope.deselect = function() {
   $scope.luas_stops_english = "";
 }
 
-}]);﻿
+// .filter('pagination', function()   {
+//      return function(luaslist, start) {
+//        return luaslist.slice(start);       MOVE TO A CONFIG FILE OR AT THE VERY BOTTON OF FILE 
+//     }
+
+//   });
+
+}])﻿
+
+.filter('pagination', function()   {
+     return function(luaslist, start) {
+       return luaslist.slice(start);
+    }
+
+  });
